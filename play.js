fa220778a63808e721d08ad0524d8b12d69f5aa7
Andrew McKinlay <apmckinlay@gmail.com>
2015-06-06 13:50:36 -0600

    changed play.ts to play.js
    to get around “require” name clash between TypeScript 1.5 and node.js

---
+++ play.js
@@ -0,0 +1,27 @@
+/**
+ * Created by andrew on 2015-05-28.
+ */
+"use strict";
+require(['./su'], function (su) {
+    function run() {
+        var src = document.getElementById("su_source").value;
+        var req = new XMLHttpRequest();
+        req.onload = results;
+        req.open("post", "/run", true);
+        req.send(src);
+    }
+    function results() {
+        var result = JSON.parse(this.responseText);
+        document.getElementById("su_result").value = result.su_result;
+        document.getElementById("js_source").value = result.js_source;
+        var js_result;
+        try {
+            js_result = eval(result.js_source + '\nf();');
+        }
+        catch (e) {
+            js_result = e;
+        }
+        document.getElementById("js_result").value = js_result;
+    }
+    document.getElementById('run').onclick = run;
+});
