3a036586d8ed300941ef84f7497bc2a9fb62ab07
Hao Xie <xiehaom7@gmail.com>
2016-05-01 21:50:21 -0600

    Made a wrapper to simplify the creation of an editor with default settings

---
+++ CodeMirror/Suneido-editor.js
@@ -0,0 +1,87 @@
+function SuneidoEditor(container, cusConfigs){
+	"use strict";
+	function getDefault(member){
+		return cusConfigs[member] || defconfigs[member];
+	}
+	
+	function initModeAndTheme(){
+		cm.setOption("mode", getDefault("mode"));
+		cm.setOption("theme", getDefault("theme"));
+	}
+	
+	function initExtraKeys(){
+		cm.setOption("extraKeys", getDefault("extraKeys"));
+	}
+	
+	function initStyle(){
+		cm.setOption("lineNumbers", getDefault("lineNumbers"));
+		cm.setOption("lineWrapping", getDefault("lineWrapping"));
+		cm.setOption("indentUnit", getDefault("indentUnit"));
+		cm.setOption("indentWithTabs", getDefault("indentWithTabs"));
+		cm.setOption("matchBrackets", getDefault("matchBrackets"));
+		cm.setOption("foldGutter", getDefault("foldGutter"));
+		cm.setOption("gutters", getDefault("gutters"));
+		cm.setOption("autoCloseBrackets", getDefault("autoCloseBrackets"));
+		cm.setOption("highlightWords", getDefault("highlightWords"));
+		cm.setOption("styleActiveLine", getDefault("styleActiveLine"));
+	}
+	
+	function initAutoCompletion(){
+		cm.setOption("autoCompletion", getDefault("autoCompletion"));
+	}
+	var defconfigs = {
+		mode: "suneido",
+		theme: "suneido",
+		lineNumbers: true,
+		lineWrapping: true,
+		indentUnit: 4,
+		indentWithTabs: true,
+		extraKeys: {"Ctrl-Q": function (cm) { cm.foldCode(cm.getCursor()); },
+					"Ctrl-F": "find",
+					"F3": "findNext",
+					"Shift-F3": "findPrev",
+					"Ctrl-H": "replace",
+					"Shift-Ctrl-H": "replaceAll",
+					"F8": "replaceCurrent",
+					"Ctrl-/": "toggleComment",
+					"Shift-Ctrl-/": function (cm) {
+				var from = cm.getCursor("from"),
+					to = cm.getCursor("to");
+				if (cm.uncomment(from, to) === false) {
+					cm.blockComment(from, to, {fullLines: false});
+				}
+			}},
+		matchBrackets : true,
+		foldGutter: true,
+		gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+		highlightWords : true,
+		autoCloseBrackets: true,
+		styleActiveLine: true,
+		autoCompletion: true
+	},
+	cm,
+	$editor,
+	$textarea;
+	
+	$editor = $("<div>", {class: "suneido-editor"});
+	$textarea = $("<textarea>");
+	$textarea.get(0).value = "//Suneido web Editor\r\n";
+	$textarea.appendTo($editor);
+	$editor.appendTo(container);
+	cm = CodeMirror.fromTextArea($textarea.get(0));
+	
+	initModeAndTheme();
+	initStyle();
+	initExtraKeys();
+	initAutoCompletion();
+	
+	$editor.on("keydown", ".CodeMirror-dialog",function(e){
+		if(e.ctrlkey || (e.keyCode <= 123 && e.keyCode >= 112)){
+			cm.triggerOnKeyDown(e);
+			return false;
+		}
+		return true;
+	});
+	
+	return $editor.get(0);
+}
