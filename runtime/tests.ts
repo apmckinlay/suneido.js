c6bbc4256529f9071e5428d31f4ca6cbf714464d
Andrew McKinlay <apmckinlay@gmail.com>
2016-07-08 14:06:29 -0600

    rename sustring to stringmethods

--- runtime/tests.ts
+++ runtime/tests.ts
@@ -3,7 +3,7 @@ import "./dnum_test";
 import "./su_test";
 import "./sudate_test";
 import "./suobject_test";
-import "./sustring_test";
+import "./stringmethods_test";
 import "./utility_test";
 console.log("finished");
 process.exit();
