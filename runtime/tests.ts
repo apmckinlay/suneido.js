221093a6b0764ce8ececcbed4f8c04031eb153fa
Andrew McKinlay <apmckinlay@gmail.com>
2016-07-27 07:31:40 -0700

    cosmetic

--- runtime/tests.ts
+++ runtime/tests.ts
@@ -2,13 +2,22 @@
 
 // import will run code
 import "./porttests_test";
+console.log("dnum_test");
 import "./dnum_test";
+console.log("su_test");
 import "./su_test";
+console.log("sudate_test");
 import "./sudate_test";
+console.log("suobject_test");
 import "./suobject_test";
+console.log("stringmethods_test");
 import "./stringmethods_test";
+console.log("tr_test");
 import "./tr_test";
+console.log("lexer_test");
 import "./lexer_test";
+console.log("charmatcher_test");
+import "./charmatcher_test";
 
 console.log("finished");
 process.exit();
