c7487b69761a7f4f6dc797d43a4cecd04af27b7c
Andrew McKinlay <apmckinlay@gmail.com>
2016-08-31 15:19:12 -0600

    minor improvements to play

--- play_samples.js
+++ play_samples.js
@@ -13,4 +13,20 @@ f(100)`,
         {
         }
     }`,
+
+'MapN':
+`f = function (s) { s $ '|' }
+'hello world'.MapN(2, f)`,
+
+'slice object':
+`x = #(1, 2, 3, 4)
+x[1 :: 2]`,
+
+'slice string':
+`s = "123456789"
+s[1 .. -1]`,
+
+'params':
+`f = function (a, b='', c=1) { }
+f.Params()`,
 }
