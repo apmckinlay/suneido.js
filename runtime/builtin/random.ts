78390d552e8c6c3cb3f9ab17574864844df8e734
Hao Xie <xie@axonsoft.com>
2018-03-30 15:11:20 -0600

    Added Random

---
+++ runtime/builtin/random.ts
@@ -0,0 +1,23 @@
+import { toInt } from "../ops";
+import { SuObject } from "../suobject";
+import { maxargs, mandatory } from "../su";
+import * as util from "../utility";
+
+export function su_random(a: any = mandatory()): number {
+    maxargs(1, arguments.length);
+    let n = toInt(a);
+    return n === 0 ? 0 : Math.floor(Math.random() * n);
+}
+
+//BUILTIN Random(n)
+//GENERATED start
+(su_random as any).$call = su_random;
+(su_random as any).$callNamed = function ($named: any, n: any) {
+    ({ n = n } = $named);
+    return su_random(n);
+};
+(su_random as any).$callAt = function (args: SuObject) {
+    return (su_random as any).$callNamed(util.mapToOb(args.map), ...args.vec);
+};
+(su_random as any).$params = 'n';
+//GENERATED end
