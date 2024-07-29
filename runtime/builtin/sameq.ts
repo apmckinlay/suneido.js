72b0b1b0964b1a171a026b0982d2922f0cbc42ee
Hao <xie@axonsoft.com>
2024-07-29 13:18:54 -0600

    Added Cmp builtin function (needed by function By)

--- runtime/builtin/sameq.ts
+++ runtime/builtin/sameq.ts
@@ -1,4 +1,5 @@
 import { SuObject } from '../suobject';
+import { cmp } from '../cmp';
 import * as util from '../utility';
 import { maxargs, mandatory } from "../args";
 
@@ -21,4 +22,26 @@ export function su_sameq(x: any = mandatory(), y: any = mandatory()): boolean {
 (su_sameq as any).$callableType = "BUILTIN";
 (su_sameq as any).$callableName = "Same?";
 (su_sameq as any).$params = 'x, y';
+//GENERATED end
+
+
+export function su_cmp(x: any = mandatory(), y: any = mandatory()): number {
+    maxargs(2, arguments.length);
+    return cmp(x, y);
+}
+
+//BUILTIN Cmp(x, y)
+//GENERATED start
+(su_cmp as any).$call = su_cmp;
+(su_cmp as any).$callNamed = function ($named: any, x: any, y: any) {
+    maxargs(3, arguments.length);
+    ({ x = x, y = y } = $named);
+    return su_cmp(x, y);
+};
+(su_cmp as any).$callAt = function (args: SuObject) {
+    return (su_cmp as any).$callNamed(util.mapToOb(args.map), ...args.vec);
+};
+(su_cmp as any).$callableType = "BUILTIN";
+(su_cmp as any).$callableName = "Cmp";
+(su_cmp as any).$params = 'x, y';
 //GENERATED end
